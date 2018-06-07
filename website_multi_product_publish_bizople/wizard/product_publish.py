3b9e8ddd4bc589a6095216b2f8b92d8246e87fdc
bizople <bizoples@gmail.com>
2018-06-07 22:35:10 +0530

    [ADD] Version 9.0

--- website_multi_product_publish_bizople/wizard/product_publish.py
+++ website_multi_product_publish_bizople/wizard/product_publish.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo Module Developed by Bizople Solutions Pvt. Ltd.
 # See LICENSE file for full copyright and licensing details.
-from odoo import models, fields, api
+from openerp import api, fields, models, _
 
 
 class ProductPublishedUnPublishedWizard(models.TransientModel):
